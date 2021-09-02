c39e1ac9fd892cc2d71a0399fbd8f12412c7a596
xyh <412805924@qq.com>
2021-09-02 22:29:05 +0800

    20210902

---
+++ 剑指 Offer 22. 链表中倒数第k个节点.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+class Solution {
+public:
+    ListNode* getKthFromEnd(ListNode* head, int k) {
+        ListNode *first = head;
+        ListNode *second = head;
+        for (int i=0; i < k; i++) {
+            if(first == NULL) {
+                return NULL;
+            }
+            first = first->next;
+        }
+        while (first != NULL) {
+            first = first->next;
+            second = second->next;
+        }
+        return second;
+    }
+};
